5acd1407cf3c088bac71809d2a0053086f6fde78
AustinPaley <austinpaley32@gmail.com>
2018-05-08 16:15:33 -0400

    Monster Difficulty Private Methods

--- app/controllers/encounters_controller.rb
+++ app/controllers/encounters_controller.rb
@@ -17,6 +17,12 @@ class EncountersController < ApplicationController
     @encounter = Encounter.new
     @monsters = Monster.all
     @characters = Character.all
+    very_easy_monsters
+    easy_monsters
+    medium_monsters
+    hard_monsters
+    very_hard_monsters
+    impossible_monsters
   end
 
   def create
@@ -80,4 +86,27 @@ class EncountersController < ApplicationController
     @encounters = Encounter.where(Encounter.arel_table[:name].lower.matches(params[:search_term].downcase))
   end
 
+  def very_easy_monsters
+    @very_easy_monsters = Monster.where(challenge_rating: 0)
+  end
+
+  def easy_monsters
+    @easy_monsters = Monster.where(challenge_rating: [1, 2, 3])
+  end
+
+  def medium_monsters
+    @medium_monsters = Monster.where(challenge_rating: [4, 5, 6, 7])
+  end
+
+  def hard_monsters
+    @hard_monsters = Monster.where(challenge_rating: [8, 9, 10])
+  end
+
+  def very_hard_monsters
+    @very_hard_monsters = Monster.where(challenge_rating: [11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+  end
+
+  def impossible_monsters
+    @impossible_monsters = Monster.where(challenge_rating: [21, 22, 23, 24, 25, 26, 27, 28, 29, 30])
+  end
 end
