be791f9cbe676e8372076f9e62b8d645d3e4f579
LaneMiller <Chris.Lane.Miller@gmail.com>
2018-05-10 15:59:43 -0400

    Finished encounter create with CR calculation

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -3,7 +3,10 @@ class Monster < ApplicationRecord
   has_many :encounters, through: :monster_encounters
   belongs_to :user, optional: true
 
-  validates :name, :description, :hit_points, :size, :challenge_rating, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, :armor_class, presence: true
+  # validates :name, :description, :hit_points, :size, :challenge_rating, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, :armor_class, presence: true
   validates :hit_points, inclusion: { in: 1..500, message: "value must be between 1 and 500."}
 
+  def quantity_by_encounter(encounter)
+    encounter.monster_encounters.find_by(monster_id: self.id).quantity
+  end
 end
